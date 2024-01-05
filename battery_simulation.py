9af03c97fdb4c5ccf412a3c1c8d846b0db0c38ab
chicotobi <tobias310788@googlemail.com>
2024-01-05 08:44:50 +0100

    late commit of old stuff

--- battery_simulation.py
+++ battery_simulation.py
@@ -1,51 +1,64 @@
-from numpy import array
+from numpy import array, zeros
 from scipy import sparse
-from scipy.sparse import linalg
 import pypardiso
+import time
 
-nx = 100
-ny = 1
-nz = 1
+nx = 60
+ny = 60
+nz = 60
+n = nx*ny*nz
 
 def f_idx(nx,ny,nz):
   def f(x,y,z):
     x = x%nx
     y = y%ny
     z = z%nz
-    return z*ny*nx + y*nx + z
+    return z*ny*nx + y*nx + x
   return f
 
 idx = f_idx(nx,ny,nz)
 
+tau = 0.0001
+h = 1/nx
+
 i = []
 j = []
 v = []
+b = zeros((n))
 for x in range(nx):
   for y in range(ny):
     for z in range(nz):
       i += [idx(x,y,z)]
-
-for k in range(1,n-1):
-  i += [k,k,k]
-  j += [k-1,k,k+1]
-  v += [1,-2,1]
-i += [n-1,n-1]
-j += [n-2,n-1]
-v += [1,-1]
+      j += [idx(x,y,z)]
+      v += [1+6*tau/h/h]
+      
+      i += [idx(x,y,z)]*6
+      j += [idx(x+1,y,z)]
+      j += [idx(x-1,y,z)]
+      j += [idx(x,y+1,z)]
+      j += [idx(x,y-1,z)]
+      j += [idx(x,y,z+1)]
+      j += [idx(x,y,z-1)]
+      v += [-tau/h/h]*6
+      
+      if (x-nx/2)**2+(y-ny/4)**2 <= nx/5:
+        b[idx(x,y,z)] = 1
 
 i = array(i)
 j = array(j)
-v = array(v)*n
-
+v = array(v)
 A = sparse.coo_matrix((v,(i,j)),shape=(n,n)).tocsr()
 
-A.toarray()
-
-b = array(list(range(n)))/n/n
-
-x = linalg.spsolve(A, b)
+t0 = time.time()
 x2 = pypardiso.spsolve(A,b)
+print("pardiso:",time.time()-t0)
 
 import matplotlib.pyplot as plt
 
-plt.plot(x,'.-')
+if ny==1:
+  plt.plot(x,'.-')
+elif nz==1:
+  x2 = x2.reshape((nx,ny))
+  plt.imshow(x2)
+else:
+  import pyevtk
